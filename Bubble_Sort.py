d31eee4c5813968a31b7e6701420a7decb08e07b
Sun0379 <16367764+Sun0379@users.noreply.github.com>
2018-03-15 00:01:44 +0800

    使用键盘输入参数作为原始数组

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -4,8 +4,14 @@
 
 #冒泡排序
 
-data_all = [10,9,8,7,6,5,4,3,2,1]
-
+#data_all = [10,9,8,7,6,5,4,3,2,1]
+data_len = int(input("请输入数组长度："))
+#初始化数组
+data_all = list(range(data_len))
+for k in list(range(0,data_len)):
+    h = k+1
+    h = str(h)
+    data_all[k] = int(input("请输入第"+h+"个数："))
 count = 0
 count_flag = 0
 for j in range(len(data_all)):
